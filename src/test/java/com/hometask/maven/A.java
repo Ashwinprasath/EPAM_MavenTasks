c350b834d322110103b409b2304eaee8ca78309a
Ashwin <ashwin_prasath@epam.com>
2022-08-17 16:10:07 +0530

    Updated Maven tasks

---
+++ src/test/java/com/hometask/maven/A.java
@@ -0,0 +1,16 @@
+package com.hometask.maven;
+
+import org.testng.annotations.Test;
+
+public class A {
+	
+	@Test
+	private void method1() {
+		System.out.println("This is method-1");
+	}
+	
+	@Test
+	private void method2() {
+		System.out.println("This is method-2");
+	}
+}
---
+++ src/test/java/com/hometask/maven/B.java
@@ -0,0 +1,16 @@
+package com.hometask.maven;
+
+import org.testng.annotations.Test;
+
+public class B {
+
+	@Test
+	private void method3() {
+		System.out.println("This is method-3");
+	}
+	
+	@Test
+	private void method4() {
+		System.out.println("This is method-4");
+	}
+}
